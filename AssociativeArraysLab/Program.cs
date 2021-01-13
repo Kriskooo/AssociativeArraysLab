871f20c936812d606d819ad20de1fd4e5710f51d
Kriskooo <kris.danielov@gmail.com>
2021-01-13 15:59:42 +0200

    AssociativeArraysLab

---
+++ AssociativeArraysLab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AssociativeArraysLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+            SortedDictionary<double, int> counts = new SortedDictionary<double, int>();
+            foreach (var number in numbers)
+            {
+                if (counts.ContainsKey(number))
+                {
+                    counts[number]++;
+                }
+                else
+                {
+                    counts.Add(number, 1);
+                }
+            }
+            foreach (var number in counts)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
---
+++ Largest3Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).OrderByDescending(n => n).Take(3).ToList();
+
+            Console.WriteLine(string.Join(' ',list));
+            
+
+            //var list = new List<int>()
+            //{
+
+            //};
+            //var result = list
+            //    .Skip()
+            //    .Take()
+            //    .ToList();
+            //foreach (var number in result)
+            //{
+            //}
+        }
+    }
+}
